8e9b849a24e8ed70ee4090e0195db78c11c1adb3
SOLOMON-Q <75553782+SOLOMON-Q@users.noreply.github.com>
2022-04-22 00:13:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,25 +12,19 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
 const path = require('path');
-const fs = require('fs');
-const stripe = require('stripe')(stripeSecretKey);
 
 const app = express();
-app.use(express.json());
-
-
-
-const storeItems = new Map([])
-
-
-
-
-
-
+const fs = require('fs');
+const stripe = require('stripe')(stripeSecretKey);
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static('public'))
 app.use(express.static('frontend'))
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(express.json());
 
 
 
@@ -46,9 +40,6 @@ mongoose.connect(db,{ useNewUrlParser:true})
 .then(()=>console.log('MongoDB Connected ...'))
 .catch(err=> console.log(err));
 
-//EJS
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
 
 
 //Bodyparser
@@ -74,6 +65,49 @@ app.use((req,res,next)=>{
     res.locals.error = req.flash('error');
     next();
 });
+app.get('/store', function(req, res) {
+  fs.readFile('items.json', function(error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      res.render('store.ejs', {
+        stripePublicKey: stripePublicKey,
+        items: JSON.parse(data)
+      })
+    }
+  })
+})
+
+app.post('/purchase', function(req, res) {
+  fs.readFile('items.json', function(error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      const itemsJson = JSON.parse(data)
+      const itemsArray = itemsJson.wigs.concat(itemsJson.merch)
+      let total = 0
+      req.body.items.forEach(function(item) {
+        const itemJson = itemsArray.find(function(i) {
+          return i.id == item.id
+        })
+        total = total + itemJson.price * item.quantity
+      })
+
+      stripe.charges.create({
+        amount: total,
+        source: req.body.stripeTokenId,
+        currency: 'usd'
+      }).then(function() {
+        console.log('Charge Successful')
+        res.json({ message: 'Successfully purchased items' })
+      }).catch(function() {
+        console.log('Charge Fail')
+        res.status(500).end()
+      })
+    }
+  })
+})
+
 
 
 //Routes
